f163be20c0b2fed4e9d8e92e2e2b6797fef6aaf3
kevstessens <kevstessens@gmail.com>
2013-06-29 18:14:15 -0300

    beginnnn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docnet::Application.config.secret_token = 'fef51ba7c6ba52690d5ddf55b0d3fe9083084fb8aebe89cb0a6013b068c72169d0278dfd4cabc71553b3752c01a8c2a26ba2d4f869c1be068b74ad84f6a1d593'
